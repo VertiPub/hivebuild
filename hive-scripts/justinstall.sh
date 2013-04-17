d591a0721fcdd79aeb575c34c83e6cd90872c658
Lee T. Tucker <ltucker@verticloud.com>
2013-04-17 13:32:46 -0700

    handle the version number better, and use a symlink for conf

--- hive-scripts/justinstall.sh
+++ hive-scripts/justinstall.sh
@@ -2,17 +2,16 @@
 # deal with the hive artifacts to create a tarball
 RPM_VERSION=0.1.0
 # have to add a 0 since hive doesn't make it's branch names match it's versions
-NAMED_VERSION=${ARTIFACT_VERSION}.0
 
 # convert each tarball into an RPM
 DEST_ROOT=${INSTALL_DIR}/opt
 mkdir --mode=0755 -p ${DEST_ROOT}
 cd ${DEST_ROOT}
-tar -xvzpf ${WORKSPACE}/hive/build/hive-${NAMED_VERSION}.tar.gz
+tar -xvzpf ${WORKSPACE}/hive/build/hive-${ARTIFACT_VERSION}.tar.gz
 mkdir --mode=0755 -p ${INSTALL_DIR}/etc
-mv ${INSTALL_DIR}/opt/hive-${NAMED_VERSION}/conf ${INSTALL_DIR}/etc/hive-${NAMED_VERSION}
-cd ${INSTALL_DIR}/opt/hive-${NAMED_VERSION}
-ln -s /etc/hive-${NAMED_VERSION} conf
+mv ${INSTALL_DIR}/opt/hive-${ARTIFACT_VERSION}/conf ${INSTALL_DIR}/etc/hive-${ARTIFACT_VERSION}
+cd ${INSTALL_DIR}/opt/hive-${ARTIFACT_VERSION}
+ln -s /etc/hive-${ARTIFACT_VERSION} conf
 
 # convert all the etc files to config files
 cd ${INSTALL_DIR}
