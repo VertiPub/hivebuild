b0d714f8f4ae5c9fa5b380029b2f1950938d7eed
Lee T. Tucker <ltucker@verticloud.com>
2013-04-17 12:36:29 -0700

    move the config to etc, make sure config files are treated as such

--- hive-scripts/justinstall.sh
+++ hive-scripts/justinstall.sh
@@ -10,7 +10,18 @@ mkdir --mode=0755 -p ${DEST_ROOT}
 cd ${DEST_ROOT}
 tar -xvzpf ${WORKSPACE}/hive/build/hive-${NAMED_VERSION}.tar.gz
 mkdir --mode=0755 -p ${INSTALL_DIR}/etc
-cp -rp ${INSTALL_DIR}/opt/hive-${NAMED_VERSION}/conf ${INSTALL_DIR}/etc/hive-${NAMED_VERSION}
+mv ${INSTALL_DIR}/opt/hive-${NAMED_VERSION}/conf ${INSTALL_DIR}/etc/hive-${NAMED_VERSION}
+cd ${INSTALL_DIR}/opt/hive-${NAMED_VERSION}
+ln -s /etc/hive-${NAMED_VERSION} conf
+
+# convert all the etc files to config files
+cd ${INSTALL_DIR}
+export CONFIG_FILES=""
+find etc -type f -print | awk '{print "/" $1}' > /tmp/$$.files
+for i in `cat /tmp/$$.files`; do CONFIG_FILES="--config-files $i $CONFIG_FILES "; done
+export CONFIG_FILES
+rm -f /tmp/$$.files
+
 
 cd ${RPM_DIR}
 
@@ -26,6 +37,7 @@ fpm --verbose \
 -n ${RPM_NAME} \
 -v ${RPM_VERSION} \
 --iteration ${DATE_STRING} \
+${CONFIG_FILES} \
 --rpm-user root \
 --rpm-group root \
 -C ${INSTALL_DIR} \
