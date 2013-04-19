20787ac7b03e3f0be09059a9cbdd0ff5872ec17e
Lee T. Tucker <ltucker@verticloud.com>
2013-04-19 08:25:01 -0700

    change the build to replace the jarred hive-log4j.properties, this is poor design by hive

--- hive-scripts/justbuild.sh
+++ hive-scripts/justbuild.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -ex
 # build the artifacts using the hive ant build
 
+cp ${WORKSPACE}/hive-scripts/hive-log4j.properties ${WORKSPACE}/hive/common/src/java/conf/hive-log4j.properties
 ant clean package tar -Dhadoop.version=2.0.2-alpha -Dhadoop-0.23.version=2.0.2-alpha -Dhadoop.mr.rev=23 -Dversion=${ARTIFACT_VERSION}
