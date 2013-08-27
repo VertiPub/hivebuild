d2fbe373e7932691d095adc4816ddd980387574b
Lee T. Tucker <ltucker@verticloud.com>
2013-08-27 07:51:05 -0700

    remove unecessary logging files, and fix the build options to make hcatalog also build with hadoop-23

--- hive-scripts/justbuild11.sh
+++ hive-scripts/justbuild11.sh
@@ -3,7 +3,4 @@
 
 #refixing wb-803 for wb-505, this would need to be a community patch, and this is  already changed in hive-0.11
 export ANT_OPTS="-XX:MaxPermSize=512m -Xmx1024m"
-cp ${WORKSPACE}/hive-scripts/data.hive-exec-log4j.properties ${WORKSPACE}/hive/data/conf/hive-log4j.properties
-cp ${WORKSPACE}/hive-scripts/ql.hive-exec-log4j.properties ${WORKSPACE}/hive/ql/src/java/conf/hive-exec-log4j.properties
-cp ${WORKSPACE}/hive-scripts/hive-log4j.properties ${WORKSPACE}/hive/common/src/java/conf/hive-log4j.properties
-ant clean package tar -Dhadoop.version=2.0.2-alpha -Dhadoop-0.23.version=2.0.2-alpha -Dhadoop.mr.rev=23
+ant clean package tar -Dhadoop.version=2.0.2-alpha -Dhadoop-0.23.version=2.0.2-alpha -Dhadoop.mr.rev=23 -Dmvn.hadoop.profile=hadoop23 -Dhadoop23.version=2.0.2-alpha
