aa5d99e35d9f4ad69b59f91379c00f507930c569
Lee T. Tucker <ltucker@altiscale.com>
2013-12-19 15:12:24 -0800

    /g missed

--- scripts/justbuildpost10.sh
+++ scripts/justbuildpost10.sh
@@ -6,4 +6,4 @@ export HADOOP_VERSION=${HADOOP_VERSION:-2.0.5-alpha}
 
 #refixing wb-803 for wb-505, this would need to be a community patch, and this is  already changed in hive-0.11
 export ANT_OPTS="-XX:MaxPermSize=512m -Xmx1024m"
-ant clean package tar -Dhadoop.version=${HADOOP_VERSION} -Dhadoop-0.23.version=2.0.2-alpha -Dhadoop.mr.rev=23 -Dmvn.hadoop.profile=hadoop23 -Dhadoop23.version=2.0.2-alpha ${ADDITIONAL_OPTIONS}
+ant clean package tar -Dhadoop.version=${HADOOP_VERSION} -Dhadoop-0.23.version=${HADOOP_VERSION} -Dhadoop.mr.rev=23 -Dmvn.hadoop.profile=hadoop23 -Dhadoop23.version=${HADOOP_VERSION} ${ADDITIONAL_OPTIONS}
