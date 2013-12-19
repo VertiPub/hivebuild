99df3ebac03dfb7de7a4641aa928ccddfe0c59fb
Lee T. Tucker <ltucker@altiscale.com>
2013-12-19 08:59:39 -0800

    update the scripts to work for both 11 and 12 and rename to post10

--- scripts/justbuild12.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -ex
-# build the artifacts using the hive ant build
-
-#refixing wb-803 for wb-505, this would need to be a community patch, and this is  already changed in hive-0.12
-export ANT_OPTS="-XX:MaxPermSize=512m -Xmx1024m"
-ant clean package tar -Dhadoop.version=2.0.2-alpha -Dhadoop-0.23.version=2.0.2-alpha -Dhadoop.mr.rev=23 -Dmvn.hadoop.profile=hadoop23 -Dhadoop23.version=2.0.2-alpha
